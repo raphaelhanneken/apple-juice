aaf405df4aa14edf3a19a88ccdccb61140f78ffd
Raphael Hanneken <raphaelhanneken@me.com>
2016-01-13 11:46:47 +0100

    Rearrange project files

--- Apple Juice/AppDelegate.swift
+++ Apple Juice/AppDelegate.swift
@@ -21,7 +21,4 @@ class AppDelegate: NSObject, NSApplicationDelegate {
   func applicationWillTerminate(aNotification: NSNotification) {
     // Insert code here to tear down your application
   }
-
-
 }
-
