9b4fe5a6496cdb872189c126fa3137ce109e8d28
Raphael Hanneken <raphaelhanneken@me.com>
2016-01-13 11:42:29 +0100

    Initial commit

---
+++ Apple Juice/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Apple Juice
+//
+//  Created by Raphael Hanneken on 13.01.16.
+//  Copyright © 2016 Raphael Hanneken. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+  @IBOutlet weak var window: NSWindow!
+
+
+  func applicationDidFinishLaunching(aNotification: NSNotification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(aNotification: NSNotification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
