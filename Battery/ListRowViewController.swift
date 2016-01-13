463a7ca76c9e3fca8b87f6664e0c018c93d54e8c
Raphael Hanneken <raphaelhanneken@me.com>
2016-01-13 22:27:15 +0100

    Add Today Widget template

---
+++ Battery/ListRowViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ListRowViewController.swift
+//  Battery
+//
+//  Created by Raphael Hanneken on 13.01.16.
+//  Copyright © 2016 Raphael Hanneken. All rights reserved.
+//
+
+import Cocoa
+
+class ListRowViewController: NSViewController {
+
+    override var nibName: String? {
+        return "ListRowViewController"
+    }
+
+    override func loadView() {
+        super.loadView()
+
+        // Insert code here to customize the view
+    }
+
+}
