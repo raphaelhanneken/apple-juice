40bb1c855eb6bc8fdf06a2ff5e10d2d5bd6fe0e9
Raphael Hanneken <raphaelhanneken@me.com>
2016-01-13 15:24:38 +0100

    Implement StatusIcon class

--- Apple Juice/StatusIcon.swift
+++ Apple Juice/StatusIcon.swift
@@ -28,5 +28,64 @@
 import Cocoa
 
 class StatusIcon {
-  
+  /// A little offset to draw the capacity bar in the correct position.
+  private static let capacityBarOffset: CGFloat = 2.0
+
+  /// Returns the charged and plugged battery image.
+  static var batteryChargedAndPlugged: NSImage? {
+    return StatusIcon.batteryImage(named: "BatteryChargedAndPlugged")
+  }
+
+  /// Returns the charging battery image.
+  static var batteryCharging: NSImage? {
+    return StatusIcon.batteryImage(named: "BatteryCharging")
+  }
+
+  ///  Draws a battery icon based on the current percentage charge of the battery.
+  ///
+  ///  - parameter percentage: The current percentage charge of the battery.
+  ///  - returns: The battery icon based on the given parameters.
+  static func batteryDischarging(currentPercentage percentage: Int) -> NSImage? {
+    // Get the images to draw the battery icon.
+    guard let batteryOutline = StatusIcon.batteryImage(named: "BatteryOutline"),
+      batteryLeft  = StatusIcon.batteryImage(named: "BatteryLevelCapB-L"),
+      batteryRight = StatusIcon.batteryImage(named: "BatteryLevelCapB-R"),
+      batteryMid   = StatusIcon.batteryImage(named: "BatteryLevelCapB-M") else {
+        return nil
+    }
+
+    let capacityBarHeight    = batteryLeft.size.height
+    let capacityBarOffsetTop = batteryOutline.size.height - capacityBarOffset - capacityBarHeight
+    var capacityBarLength    = CGFloat(Double(percentage) / 13) * batteryMid.size.width
+
+    if batteryMid.size.width >= capacityBarLength {
+      capacityBarLength = (2 * batteryMid.size.width) + 0.1
+    }
+    let drawingRect = NSRect(x: capacityBarOffset, y: capacityBarOffsetTop,
+      width: capacityBarLength, height: capacityBarHeight)
+
+    batteryOutline.lockFocus()
+    NSDrawThreePartImage(drawingRect, batteryLeft, batteryMid, batteryRight, false,
+      .CompositeCopy, 1, false)
+    batteryOutline.unlockFocus()
+
+    return batteryOutline
+  }
+
+  ///  Retrieves the battery image for the supplied image name.
+  ///
+  ///  - parameter name: Name of the image.
+  ///  - returns: The image.
+  private static func batteryImage(named name: String) -> NSImage? {
+    // Define the path to apple's battery icons.
+    let path = "/System/Library/CoreServices/Menu Extras/Battery.menu/Contents/Resources/"
+
+    // Open the supplied file as NSImage.
+    if let img = NSImage(contentsOfFile: "\(path)\(name).pdf") {
+      return img
+    } else {
+      print("An error occured while reading image named: \(name)")
+      return nil
+    }
+  }
 }
