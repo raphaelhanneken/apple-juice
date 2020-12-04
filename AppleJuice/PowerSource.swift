b434d6c2cc8d83686a7fd043601aa6f24fbd96a4
Raphael Hanneken <raphaelhanneken@me.com>
2020-12-04 17:41:33 +0100

    translate the power source

--- AppleJuice/PowerSource.swift
+++ AppleJuice/PowerSource.swift
@@ -10,4 +10,8 @@ enum PowerSource: String {
     case unknown = "Unknown"
     case powerAdapter = "Power Adapter"
     case battery = "Battery"
+
+    var localizedDescription: String {
+        return NSLocalizedString(self.rawValue, comment: "")
+    }
 }
